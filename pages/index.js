0809a00571a4711fcb09470a45252014b5011394
thiagopereirar <thpereira.r@gmail.com>
2025-11-07 14:13:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-function Home(){
-    return <h1>Ei! Você que está lendo, gostaria de informar que você é a mulher mais incrível, mais linda do universo... sim Nathália, você é</h1>
-     
-     
+function Home() {
+  return (
+    <h1>
+      Ei! Você que está lendo, gostaria de informar que você é a mulher mais
+      incrível, mais linda do universo... sim Nathália, você é
+    </h1>
+  );
 }
 
 export default Home;
